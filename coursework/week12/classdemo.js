2cb1e4a54dae3f77c9efbc8fb65cdcd10dc68bd7
kamrynmckenna <mckek472@newschool.edu>
2022-05-06 04:05:35 -0400

    fixes

---
+++ coursework/week12/classdemo.js
@@ -0,0 +1,24 @@
+console.log(data);
+
+function handleDropdown( { console.log('target.value');})  
+
+const content= document.querySelector('.content');
+data
+
+.filter(item => {
+	return item.type === 'brass';
+})
+
+
+.forEach(item => {
+	console.log(item);
+	content.innerHTML +=
+	<div class="card">
+	<p> trending number: ${item.popularity} </p>
+
+<h3>${item.name} </h3>
+<p>${item.type}</p>
+<p class="price">$${item.price}</p>
+<p class="${item.isInStock" === true ? 'in stock': 'out of stock': ''
+
+}
---
+++ coursework/week12/script.js
@@ -0,0 +1,41 @@
+
+
+fetch('https://api.airtable.com/v0/appS8iC7fLSvvjvuQ/collection', {
+    headers: {
+        Authorization: 'Bearer keyzunI8IVzH042aJ',
+    }
+})
+.then(response => response.json())
+.then(data => {
+
+console.log(data);
+
+const collection= document.querySelector('.collection');
+
+
+data.records.forEach(piece => {
+    console.log(piece),
+    collection.innerHTML += `
+    <div class="piece">
+    <h3>${piece.fields.name}</h3> 
+    <h5>${piece.fields.type}</h5>
+    <img src='piece.fields.images[0].thumbnails.large.url}" width='200'/>
+
+
+
+`
+</div> /
+;
+
+});
+
+});
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
