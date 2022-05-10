64fe7288f8fe3d2ab2aa4136ed5396d2ec9a4a0f
kamrynmckenna <mckek472@newschool.edu>
2022-05-10 17:01:54 -0400

    p3+

--- studio/project3+/script.js
+++ studio/project3+/script.js
@@ -1,40 +1,66 @@
+$(document).ready(function() {
+  let results = {};
+  let filterValue = 'all';
+  let selectors = {};
 
+  // listen for change event on select dropdown fromHTML
+  document.querySelector('#color-filter').addEventListener('change', (event) => {
+    filterValue = event.target.value; // update filterValue with value from selected option
+    generateContent(); // after updating filter value, then re-run the content generation to caputre
+  });
 
-fetch('https://api.airtable.com/v0/appS8iC7fLSvvjvuQ/collection', {
-    headers: {
-        Authorization: 'Bearer keyzunI8IVzH042aJ',
-    }
-})
-.then(response => response.json())
-.then(data => {
+  const generateContent = () => {
+    Object.keys(results).forEach((type) => {
+      const selector = selectors[type]
+      if (selector.html()?.trim() !== '') {
+        selector.slick('unslick');
+        selector.html(''); 
+      }      
+      results[type]
+      .filter((piece) => {
+        return filterValue === 'all' || piece.fields.Color === filterValue;
+      })
+      .forEach((piece) => {
+        selector.append(
+          `<div class="item">          
+            <h4>${piece.fields.Name}</h4>
+            <img src="${piece.fields.images[0].thumbnails.large.url}" height='300' width='250' />
+          </div>`
+        )
+      });
+      selector.slick()
+    })
+  };
 
-console.log(data);
+  fetch('https://api.airtable.com/v0/appS8iC7fLSvvjvuQ/collection', {
+      headers: {
+          Authorization: 'Bearer keyzunI8IVzH042aJ',
+      }
+  })
+  .then(response => response.json())
+  .then(data => {
+    //const results = { top: [], bottom: [], accessories: [], shoes: [] };
+    results = data.records.reduce((acc, val) => {
+      if (!acc[val.fields.type]) {
+        acc[val.fields.type] = [];
+        selectors[val.fields.type] = $(`#${val.fields.type}Carousel`)
+      }
+      acc[val.fields.type].push(val);
+      return acc 
+    }, {})
 
-const collection= document.querySelector('.collection');
+    generateContent(); 
+  });
 
+  const colorButtons = [...document.querySelectorAll('.colorButton')];
+document.addEventListener('click', e => {
+    if (!colorButtons.includes(e.target)) return;
+    document.body.style.background = e.target.textContent;
 
-data.records.forEach(piece {
-    console.log(piece),
-    collection.innerHTML += 
-    <div class="piece">
-    <h3>${piece.fields.name}</h3> 
-    <h5>${piece.fields.type}</h5>
-    <img src='piece.fields.images[0].thumbnails.large.url}" width='200'/>
-
-
-
-</div>
-;
+   
 
 });
 
+
 });
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; 
-
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
